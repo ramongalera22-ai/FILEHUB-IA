46e36563b2a77d14aaeadb80a355a5765c5e6c98
D <d@f>
2026-04-01 17:09:17 +0000

    v108 + DB check page

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
